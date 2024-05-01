8ba5ae61f96f41d292f949c3830627651125fcc6
yuchormanski <yuchormanski@gmail.com>
2024-05-01 10:17:57 +0300

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Nikolay  Yuchormanski on 1.05.24.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
